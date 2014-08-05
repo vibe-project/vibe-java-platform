38ef740c146af7b5e915d05026f72e870dc081ba
Donghwan Kim <flowersinthesand@gmail.com>
2014-08-06 01:36:50 +0900

    Reorganize project Atmosphere/vibe-java-server#30

---
+++ servlet3/src/main/java/org/atmosphere/vibe/server/platform/servlet/ServletBridge.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2014 The Vibe Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.atmosphere.vibe.server.platform.servlet;
+
+import java.util.UUID;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.atmosphere.vibe.server.platform.Action;
+import org.atmosphere.vibe.server.platform.Actions;
+import org.atmosphere.vibe.server.platform.ServerHttpExchange;
+import org.atmosphere.vibe.server.platform.SimpleActions;
+
+/**
+ * Convenient class to install Servlet bridge.
+ *
+ * @author Donghwan Kim
+ */
+public class ServletBridge {
+
+    private Actions<ServerHttpExchange> httpActions = new SimpleActions<>();
+
+    public ServletBridge(ServletContext context, String path) {
+        @SuppressWarnings("serial")
+        Servlet servlet = new HttpServlet() {
+            @Override
+            protected void service(HttpServletRequest req, HttpServletResponse resp) {
+                httpActions.fire(new ServletServerHttpExchange(req, resp));
+            }
+        };
+        ServletRegistration.Dynamic reg = context.addServlet("vibe#" + UUID.randomUUID(), servlet);
+        reg.setAsyncSupported(true);
+        reg.addMapping(path);
+    }
+
+    /**
+     * Adds an action to be called on HTTP request with
+     * {@link ServerHttpExchange}.
+     */
+    public ServletBridge httpAction(Action<ServerHttpExchange> action) {
+        httpActions.add(action);
+        return this;
+    }
+
+}
---
+++ vertx2/src/test/java/org/atmosphere/vibe/server/platform/vertx/VertxServerWebSocketTest.java
@@ -0,0 +1,47 @@
+package org.atmosphere.vibe.server.platform.vertx;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
+import org.atmosphere.vibe.server.platform.Action;
+import org.atmosphere.vibe.server.platform.ServerWebSocket;
+import org.atmosphere.vibe.server.platform.test.ServerWebSocketTestTemplate;
+import org.junit.Test;
+import org.vertx.java.core.VertxFactory;
+import org.vertx.java.core.http.HttpServer;
+
+public class VertxServerWebSocketTest extends ServerWebSocketTestTemplate {
+
+    HttpServer server;
+
+    @Override
+    protected void startServer() {
+        server = VertxFactory.newVertx().createHttpServer();
+        new VertxBridge(server, "/test").websocketAction(new Action<ServerWebSocket>() {
+            @Override
+            public void on(ServerWebSocket ws) {
+                performer.serverAction().on(ws);
+            }
+        });
+        server.listen(port);
+    }
+
+    @Override
+    protected void stopServer() {
+        server.close();
+    }
+
+    @Test
+    public void unwrap() {
+        performer.serverAction(new Action<ServerWebSocket>() {
+            @Override
+            public void on(ServerWebSocket ws) {
+                assertThat(ws.unwrap(org.vertx.java.core.http.ServerWebSocket.class),
+                        instanceOf(org.vertx.java.core.http.ServerWebSocket.class));
+                performer.start();
+            }
+        })
+        .connect();
+    }
+
+}
