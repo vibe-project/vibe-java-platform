9b8fc1bb6ddb276135422d5fb77a0ca7f9f8582f
Donghwan Kim <flowersinthesand@gmail.com>
2014-09-13 16:45:03 +0900

    Use ugly hack to fix Atmosphere/atmosphere#1623

--- atmosphere2/src/main/java/org/atmosphere/vibe/platform/server/atmosphere2/AtmosphereBridge.java
+++ atmosphere2/src/main/java/org/atmosphere/vibe/platform/server/atmosphere2/AtmosphereBridge.java
@@ -15,15 +15,17 @@
  */
 package org.atmosphere.vibe.platform.server.atmosphere2;
 
+import static org.atmosphere.websocket.WebSocketEventListener.WebSocketEvent.TYPE.CLOSE;
+
 import java.io.IOException;
 import java.util.UUID;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 import org.atmosphere.cpr.ApplicationConfig;
 import org.atmosphere.cpr.AtmosphereFramework;
+import org.atmosphere.cpr.AtmosphereObjectFactory;
 import org.atmosphere.cpr.AtmosphereResource;
 import org.atmosphere.cpr.AtmosphereResource.TRANSPORT;
 import org.atmosphere.cpr.AtmosphereServlet;
@@ -33,6 +35,10 @@ import org.atmosphere.vibe.platform.Actions;
 import org.atmosphere.vibe.platform.SimpleActions;
 import org.atmosphere.vibe.platform.server.ServerHttpExchange;
 import org.atmosphere.vibe.platform.server.ServerWebSocket;
+import org.atmosphere.websocket.DefaultWebSocketProcessor;
+import org.atmosphere.websocket.WebSocket;
+import org.atmosphere.websocket.WebSocketEventListener;
+import org.atmosphere.websocket.WebSocketProcessor;
 
 /**
  * Convenient class to install Atmosphere bridge.
@@ -45,12 +51,8 @@ public class AtmosphereBridge {
     private Actions<ServerWebSocket> wsActions = new SimpleActions<>();
 
     public AtmosphereBridge(ServletContext context, String path) {
-        AtmosphereServlet servlet = null;
-        try {
-            servlet = context.createServlet(AtmosphereServlet.class);
-        } catch (ServletException e) {
-            throw new RuntimeException(e);
-        }
+        // Configure Atmosphere
+        AtmosphereServlet servlet = new AtmosphereServlet();
         AtmosphereFramework framework = servlet.framework();
         framework.addAtmosphereHandler("/", new AtmosphereHandlerAdapter() {
             @Override
@@ -64,7 +66,21 @@ public class AtmosphereBridge {
                 }
             }
         });
+        framework.setWebsocketProcessorClassName(VibePlatformWebSocketProcessor.class.getName());
+        framework.objectFactory(new AtmosphereObjectFactory() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public <T, U extends T> T newClassInstance(AtmosphereFramework framework, Class<T> classType, Class<U> defaultType)
+                    throws InstantiationException, IllegalAccessException {
+                // Intercepts WebSocketProcessor's instantiation to provide framework instance
+                if (classType == WebSocketProcessor.class && defaultType == VibePlatformWebSocketProcessor.class) {
+                    return (T) new VibePlatformWebSocketProcessor(framework);
+                }
+                return defaultType.newInstance();
+            }
+        });
 
+        // Registers the servlet programmatically
         ServletRegistration.Dynamic reg = context.addServlet("vibe#" + UUID.randomUUID(), servlet);
         reg.setAsyncSupported(true);
         reg.addMapping(path);
@@ -87,6 +103,45 @@ public class AtmosphereBridge {
     public AtmosphereBridge websocketAction(Action<ServerWebSocket> action) {
         wsActions.add(action);
         return this;
+    }
+    
+    // Ugly fix for https://github.com/Atmosphere/atmosphere/issues/1623
+    @SuppressWarnings("serial")
+    public static class VibePlatformWebSocketProcessor extends DefaultWebSocketProcessor {
+
+        public VibePlatformWebSocketProcessor(AtmosphereFramework framework) {
+            super(framework);
+        }
+
+        @Override
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+        public void executeClose(WebSocket webSocket, int closeCode) {
+            // Notifies listeners first because super.asynchronousProcessor.endRequest empties them
+            boolean isClosedByClient = webSocket.resource() == null ? true : webSocket.resource().getAtmosphereResourceEvent().isClosedByClient();
+            if (!isClosedByClient) {
+                notifyListener(webSocket, new WebSocketEventListener.WebSocketEvent(closeCode, CLOSE, webSocket));
+            }
+            // call super.executeClose for the rest of things
+            super.executeClose(webSocket, closeCode);
+        }
+        
+        @Override
+        public void close(WebSocket webSocket, int closeCode) {
+            boolean done = false;
+            AtmosphereResource resource = webSocket.resource();
+            if (resource != null) {
+                // Intercepts and call executeClose only if this condition which is the case meets
+                if (!resource.getAtmosphereResourceEvent().isClosedByClient() && resource.getAtmosphereResourceEvent().isClosedByApplication() && resource.isCancelled()) {
+                    executeClose(webSocket, closeCode);
+                    done = true;
+                }
+            }
+            // If it's not our case, call super.close
+            if (!done) {
+                super.close(webSocket, closeCode);
+            }
+        }
+        
     }
 
 }
