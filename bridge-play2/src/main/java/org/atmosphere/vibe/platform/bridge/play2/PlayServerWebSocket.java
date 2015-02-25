8cd283eadc61135232a5a942f4421e0e999f4b2d
Donghwan Kim <flowersinthesand@gmail.com>
2015-02-25 15:18:05 +0900

    Fixes #45 Log error instead of throwing exception

--- bridge-play2/src/main/java/org/atmosphere/vibe/platform/bridge/play2/PlayServerWebSocket.java
+++ bridge-play2/src/main/java/org/atmosphere/vibe/platform/bridge/play2/PlayServerWebSocket.java
@@ -20,6 +20,8 @@ import java.nio.ByteBuffer;
 import org.atmosphere.vibe.platform.action.Action;
 import org.atmosphere.vibe.platform.ws.AbstractServerWebSocket;
 import org.atmosphere.vibe.platform.ws.ServerWebSocket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import play.libs.F.Callback;
 import play.libs.F.Callback0;
@@ -35,12 +37,15 @@ import play.mvc.WebSocket.Out;
  */
 public class PlayServerWebSocket extends AbstractServerWebSocket {
 
+    private final Logger log = LoggerFactory.getLogger(PlayServerWebSocket.class);
     private final Request request;
     private final WebSocket.Out<String> out;
 
     public PlayServerWebSocket(Request request, In<String> in, Out<String> out) {
         this.request = request;
         this.out = out;
+        // TODO https://github.com/vibe-project/vibe-java-platform/issues/4
+        // Supports text frame only for now
         in.onMessage(new Callback<String>() {
             @Override
             public void invoke(String message) throws Throwable {
@@ -58,7 +63,8 @@ public class PlayServerWebSocket extends AbstractServerWebSocket {
     @Override
     public ServerWebSocket binaryAction(Action<ByteBuffer> action) {
         // TODO https://github.com/vibe-project/vibe-java-platform/issues/4
-        throw new UnsupportedOperationException("Play Java API doesn't allow to receive text and binary frame together in a single connection");
+        log.error("Play Java API doesn't allow to receive text and binary frame together in a single connection");
+        return this;
     }
 
     @Override
@@ -80,7 +86,7 @@ public class PlayServerWebSocket extends AbstractServerWebSocket {
     protected void doSend(ByteBuffer byteBuffer) {
         // Transformation from binary to text results in sending a text frame not binary frame
         // TODO https://github.com/vibe-project/vibe-java-platform/issues/4
-        throw new UnsupportedOperationException("Play Java API doesn't allow to write text and binary frame together in a single connection");
+        log.error("Play Java API doesn't allow to send text and binary frame together in a single connection");
     }
 
     /**
